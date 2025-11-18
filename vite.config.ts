46ae79f77af7c97a197358b3df48c1fc031ddcaf
HieuLe <96852550+Wecare-tool@users.noreply.github.com>
2025-11-18 11:28:42 +0700

    Update API keys in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': 'AIzaSyAMPeL97mZbFmcV5lJTJd0tRHcnj_5qNYk',
+        'process.env.GEMINI_API_KEY': 'AIzaSyAMPeL97mZbFmcV5lJTJd0tRHcnj_5qNYk'
       },
       resolve: {
         alias: {
